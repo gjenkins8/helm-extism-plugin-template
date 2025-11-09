5aa791f244f6accbd7f822ef09201ab49ff38f4d
George Jenkins <gjenkins8@bloomberg.net>
2025-11-09 09:36:56 -0800

    Improve README.md and code comments (#2)
    Signed-off-by: George Jenkins <gjenkins8@bloomberg.net>

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # helm-extism-plugin-template
 
 Helm Wasm/Extism plugin template is a simple skeleton for creating a Helm Wasm plugin based on [Extism's Go PDK](https://pkg.go.dev/github.com/extism/go-pdk).
+
+## Usage
+
+```Makefile
+make build      # Build the Wasm plugin
+make test       # Run tests
+make vet        # Static analysis
+```
+
+You can also pass custom flags to the test target:
+
+```sh
+make test TEST_FLAGS="-bench ."
+```
+
+## Testing
+
+The `testdriver/` directory shows a simple driver for loading the plugin and executing tests against it.
--- main.go
+++ main.go
@@ -1,32 +1,50 @@
 package main
 
+/**
+ * Helm Wasm/Extism plugin template is a simple skeleton for creating a Helm Wasm plugin based on Extism's Go PDK.
+ * The implementation handles Extism's PDK input/output and error handling.
+ *
+ * To implement your plugin logic, modify the `replaceMeImplementationGoesHere` function below.
+ *
+ *
+ * For more details on using the Extism Go PDK, see: https://pkg.go.dev/github.com/extism/go-pdk
+ */
 import (
 	"fmt"
 
 	pdk "github.com/extism/go-pdk"
 )
 
+// Input defines the (JSON serialzable) plugin input
 type Input struct {
 	Foo string `json:"foo"`
 }
 
+// Input defines the (JSON serialzable) plugin output
 type Output struct {
 	Bar string `json:"bar"`
 }
 
-func ReplaceMeImplementationGoesHere(input Input) (Output, error) {
+// ReplaceMeImplementationGoesHere is a placeholder for the actual plugin logic
+func replaceMeImplementationGoesHere(input Input) (Output, error) {
+
+	// ##############################
+	// !! Implementation goes here !!
+	// ##############################
+
 	return Output{
 		Bar: input.Foo,
 	}, nil
 }
 
-func RunPlugin() error {
+// runPlugin wraps the plugin implementation with PDK input/output amnd error handling
+func runPlugin() error {
 	var input Input
 	if err := pdk.InputJSON(&input); err != nil {
 		return fmt.Errorf("failed to parse input json: %w", err)
 	}
 
-	output, err := ReplaceMeImplementationGoesHere(input)
+	output, err := replaceMeImplementationGoesHere(input)
 	if err != nil {
 		pdk.Log(pdk.LogError, fmt.Sprintf("failed: %s", err.Error()))
 		return err
@@ -39,12 +57,14 @@ func RunPlugin() error {
 	return nil
 }
 
+// Plugin entry point as invoked by Extism
+//
 //go:wasmexport helm_plugin
 func HelmPlugin() uint64 {
 
 	pdk.Log(pdk.LogDebug, "running plugin")
 
-	if err := RunPlugin(); err != nil {
+	if err := runPlugin(); err != nil {
 		pdk.Log(pdk.LogError, err.Error())
 		pdk.SetError(err)
 		return 1
@@ -53,4 +73,4 @@ func HelmPlugin() uint64 {
 	return 0
 }
 
-func main() {}
+func main() {} // Required for golang build (ignore)
